84db8ca6e3479581d8209778c2c771a9d6aef760
kejek <arididjj@gmail.com>
2025-12-08 07:09:15 +0700

    Delete app directory

--- app/api/auth/me/route.js
+++
@@ -1,9 +0,0 @@
-const { NextResponse } = require("next/server");
-const { getUserFromRequest } = require("../../../../lib/auth");
-
-async function GET(req) {
-  const user = getUserFromRequest(req);
-  return NextResponse.json({ user: user || null });
-}
-
-module.exports = { GET };
